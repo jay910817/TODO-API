ad8fdd30b35155a82e629b580465ae4e871c4959
jay910817 <106866435+jay910817@users.noreply.github.com>
2023-08-09 14:11:20 +0800

    加入專案檔案。

---
+++ my-todo-list/Controllers/TODOController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_todo_list.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TODOController : ControllerBase
+    {
+    }
+    public async Task<ActionResult<IEnumerable<Todo>>> GetTODOList()
+    {
+        var result = Context.Todo.ToList();
+        return Ok(result);
+    }
+    public async Task<IActionResult>AddTODO(Todo newTODO)
+    {
+        await _context.Todo.AddAsync(newTODO);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+    public async Task<ActionResult>DeleteTODO(int id)
+    {
+        var todo = _context.Todo.Where(t => t.Id == id).FirstOrDefault();
+        if (todo != null)
+        {
+            _context.Todo.Remove(todo);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        else
+        {
+            return BadRequest();
+        }
+    }
+
+
+}
---
+++ my-todo-list/Data/TodoDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace my_todo_list.Data;
+
+public partial class TodoDbContext : DbContext
+{
+    public TodoDbContext()
+    {
+    }
+
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options)
+    {
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddEntityFrameworkSqlite().AddDbContext<TodoDbContext>();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Data Source=todoDB.db;Cache=Shared");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
