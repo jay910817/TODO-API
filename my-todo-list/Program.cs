0ca2fd8dff4561ad95f7794421230a45f7561f74
seven <seven@dongfeng.com.tw>
2023-08-10 13:59:35 +0800

    fix: 增加DB連結

--- my-todo-list/Program.cs
+++ my-todo-list/Program.cs
@@ -1,3 +1,5 @@
+using my_todo_list.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//
+builder.Services.AddDbContext<todoDBContext>();
+
 
 var app = builder.Build();
 
